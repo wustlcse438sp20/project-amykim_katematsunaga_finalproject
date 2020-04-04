fdaa7e157af5a3b54f7d2f960b9ae1400bf1900e
Amy Kim <amy.kim@wustl.edu>
2020-04-04 12:47:25 -0500

    mainactivity, login/signup xmls

--- app/src/main/java/com/example/finalproject/MainActivity.kt
+++ app/src/main/java/com/example/finalproject/MainActivity.kt
@@ -1,12 +1,54 @@
 package com.example.finalproject
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.example.cse438.cse438_assignment4.R
+import com.example.cse438.cse438_assignment4.fragments.LoginFragment
+import com.example.cse438.cse438_assignment4.fragments.SignUpFragment
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragmentAdapter =
+                MyPagerAdapter(
+                        supportFragmentManager
+                )
+        viewpager_main.adapter = fragmentAdapter
+
+        tabs_main.setupWithViewPager(viewpager_main)
+    }
+
+    class MyPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+        override fun getCount() : Int {
+            return 2
+        }
+
+        override fun getItem(position: Int) : Fragment {
+            return when (position) {
+                0 -> { LoginFragment() }
+                else -> SignUpFragment()
+            }
+        }
+
+        override fun getPageTitle(position: Int): CharSequence {
+            return when (position) {
+                0 -> "Login"
+                else -> "Sign Up"
+            }
+        }
+
+
+
     }
 }
+
+
