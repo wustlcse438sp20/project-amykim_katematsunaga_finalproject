329c1766016cc1109dbb3825d7cda8b79f52e545
amy.kim <amy.kim@wustl.edu>
2020-04-04 13:09:54 -0500

    committing copied code for login and signup, contains errors

--- app/src/main/java/com/example/finalproject/MainActivity.kt
+++ app/src/main/java/com/example/finalproject/MainActivity.kt
@@ -5,9 +5,9 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
-import com.example.cse438.cse438_assignment4.R
-import com.example.cse438.cse438_assignment4.fragments.LoginFragment
-import com.example.cse438.cse438_assignment4.fragments.SignUpFragment
+import com.example.finalproject.fragments.LoginFragment
+import com.example.finalproject.fragments.SignUpFragment
+import com.example.finalproject.R
 import kotlinx.android.synthetic.main.activity_main.*
 
 
